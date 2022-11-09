1cd8b9733ffa0f3a41a50b9c1828591b2f771c27
Hüseyin ESKAN <huseyineskan01@gmail.com>
2022-11-09 02:00:32 +0100

    alarm sound added and readme edited

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# digitalclock
-This is a simple digital clock in JS
+# Digital Clock in Javascript
+
+This is a simple digital clock in JS.
+
+## Fetures
+- AM/PM Digital clock.
+- Parts of all 2 digits.
+- Only one alarm can be set.
+
+## Features that can be added
+- More alarm can be set.
+- The alarm sound can be selectable.
+- Stop button can be added.
+- API can be connected.
+
+## Screenshot
+![Digital Clock](https://github.com/huseyineskan/digitalclock/blob/main/media/digitalclock.gif)
--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    const myClock = document.getElementById('clock');
     const myHour = document.getElementById('hours');
     const myMinute = document.getElementById('minutes');
     const mySecond = document.getElementById('seconds');
     const myPeriod = document.getElementById('period');
-    
+
+    var setValues = {h:null, m:null, s:null};
     var hour, min, sec;
+
+    // ALARM --------------------------------------------
+
+    const btnAlarm = document.getElementById('btnAlarm');
+    const alarmPopup = document.querySelector('.alarm-popup');
+    const btnClosePopup = document.getElementById('close-popup');
+    const btnSetAlarm = document.getElementById('btnSetAlarm');
+    const alarmTimeText = document.getElementById('alarmTime');
     
+    const alarmHours = document.getElementById('alarm-hours');
+    const alarmMin = document.getElementById('alarm-min');
+    const alarmSec = document.getElementById('alarm-sec');
+    
+    var alarmTime;
+    
+    btnAlarm.addEventListener('click', showAlarmPopUp);
+    btnSetAlarm.addEventListener('click', setAlarm);
+    btnClosePopup.addEventListener('click', closePopUp);
+
+    // Time function
     function myTime(){
         var myDate = new Date();
         var period = "PM";
@@ -28,31 +47,18 @@ document.addEventListener('DOMContentLoaded', function(){
         myHour.textContent = `${hour}`;
         myMinute.textContent = `${min}`;
         mySecond.textContent = `${sec}`;
+
+        if(setValues.h == hour && setValues.m == min && setValues.s == sec){
+            play(); // play the alarm sound
+            alarmButton();
+        }
     
         setTimeout(myTime, 1000);
     }
     
     myTime();
     
-    // ALARM ---------------------------------------------
-    
-    const btnAlarm = document.getElementById('btnAlarm');
-    const alarmPopup = document.querySelector('.alarm-popup');
-    const btnClosePopup = document.getElementById('close-popup');
-    const btnSetAlarm = document.getElementById('btnSetAlarm');
-    const alarmTimeText = document.getElementById('alarmTime');
-    
-    const alarmHours = document.getElementById('alarm-hours');
-    const alarmMin = document.getElementById('alarm-min');
-    const alarmSec = document.getElementById('alarm-sec');
-    
-    var alarmTime;
-    var setValues = {h:null, m:null, s:null};
-    
-    btnAlarm.addEventListener('click', showAlarmPopUp);
-    btnSetAlarm.addEventListener('click', setAlarm);
-    btnClosePopup.addEventListener('click', closePopUp);
-    
+    // ALARM
     // Show the alarm popup function
     function showAlarmPopUp(){
         alarmPopup.style.display = "flex";
@@ -81,4 +87,15 @@ document.addEventListener('DOMContentLoaded', function(){
         closePopUp();
     }
 
+    function alarmButton(){
+        btnAlarm.classList.add('alarming');
+        setTimeout(() => {
+            btnAlarm.classList.remove('alarming');
+        }, 10000); // audio 10sn
+    }
+
+    function play(){
+        var audio = new Audio('../media/alarm.mp3');
+        audio.play();
+    }
 });
